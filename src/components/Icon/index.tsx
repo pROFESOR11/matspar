67d75600906d1391d51ad3701d6d4490c01fe283
Ali Cimen <ali_cimen_11@hotmail.com>
2023-02-08 23:37:18 +0300

    add Icon component - wrapper for svgr-generated sub components

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,14 @@
+import { SVGProps } from 'react'
+import * as Icons from './subComponents'
+
+export type IconName = keyof typeof Icons
+
+export type IconProps = Omit<SVGProps<SVGSVGElement>, 'ref'> & {
+  name: IconName
+}
+
+export const Icon = ({ name, ...props }: IconProps) => {
+  const Component = Icons[name]
+
+  return <Component {...props} />
+}
