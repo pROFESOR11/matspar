9e1fa855163d9bb915ef5135a6b83f6548f1f880
Ali Cimen <ali_cimen_11@hotmail.com>
2023-02-11 20:35:33 +0300

    add react-query initial setup for _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Raleway } from '@next/font/google'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { useState } from 'react'
 
 const raleway = Raleway({
   weight: ['400', '500', '600', '700'],
@@ -8,9 +10,14 @@ const raleway = Raleway({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
   return (
-    <main className={raleway.className}>
-      <Component {...pageProps} />
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <main className={raleway.className}>
+          <Component {...pageProps} />
+        </main>
+      </Hydrate>
+    </QueryClientProvider>
   )
 }
