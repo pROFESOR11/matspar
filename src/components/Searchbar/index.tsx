77d875aa78a338a8c8d246bd4ea873e12dbc42fa
Ali Cimen <ali_cimen_11@hotmail.com>
2023-02-12 00:03:43 +0300

    add onPressEnter prop for searchbar input

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -1,14 +1,33 @@
 import { Icon } from '@/components/Icon'
 import styles from './Searchbar.module.css'
 import cx from 'classnames'
+import { KeyboardEvent, useRef } from 'react'
 
-export const Searchbar = (props: JSX.IntrinsicElements['input']) => {
+type SearchbarProps = JSX.IntrinsicElements['input'] & {
+  onPressEnter?: (text: string) => void
+}
+
+export const Searchbar = ({ onPressEnter, ...props }: SearchbarProps) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      onPressEnter?.(encodeURIComponent(inputRef.current?.value ?? ''))
+      inputRef.current?.blur()
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <Icon name="Menu" className={cx(styles.icon, styles.menu_icon)} />
+      <Icon name="Menu" className={cx(styles.menu_icon)} />
       <div className={styles.input_container}>
         <Icon name="Search" className={styles.icon} />
-        <input className={styles.input} {...props} />
+        <input
+          ref={inputRef}
+          className={styles.input}
+          onKeyDown={handleKeyDown}
+          {...props}
+        />
       </div>
     </div>
   )
