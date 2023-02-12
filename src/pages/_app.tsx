2dfff4ba44a275a55d590cce9ccd1659190d6559
Ali Cimen <ali_cimen_11@hotmail.com>
2023-02-12 09:57:45 +0300

    wrap app with QueryParamProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Raleway } from '@next/font/google'
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { NextAdapter } from 'next-query-params'
+import { QueryParamProvider } from 'use-query-params'
 import { useState } from 'react'
 
 const raleway = Raleway({
@@ -12,12 +14,14 @@ const raleway = Raleway({
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <main className={raleway.className}>
-          <Component {...pageProps} />
-        </main>
-      </Hydrate>
-    </QueryClientProvider>
+    <QueryParamProvider adapter={NextAdapter}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <main className={raleway.className}>
+            <Component {...pageProps} />
+          </main>
+        </Hydrate>
+      </QueryClientProvider>
+    </QueryParamProvider>
   )
 }
