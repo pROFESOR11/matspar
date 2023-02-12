0d1a3a79e37484ec6b1a0dea38860cd4c1518ac8
Ali Cimen <ali_cimen_11@hotmail.com>
2023-02-12 10:39:38 +0300

    add Spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,12 @@
+import styles from './spinner.module.css'
+
+export const Spinner = () => {
+  return (
+    <div className={styles.lds_ring}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
