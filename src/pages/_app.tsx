d6d0e8d4da5edb5882a0ebeccd4f682f192f147d
Ali Cimen <ali_cimen_11@hotmail.com>
2023-02-12 20:55:25 +0300

    wrap app with default seo provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,12 @@ import '@/styles/globals.css'
 import { Raleway } from '@next/font/google'
 import type { AppProps } from 'next/app'
 import { NextAdapter } from 'next-query-params'
+import { DefaultSeo } from 'next-seo'
 import { useState } from 'react'
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 import { QueryParamProvider } from 'use-query-params'
+
+import defaultSeo from '../../next-seo.config'
 
 const raleway = Raleway({
   weight: ['400', '500', '600', '700'],
@@ -18,6 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <QueryParamProvider adapter={NextAdapter}>
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
+          <DefaultSeo {...defaultSeo} />
           <main className={raleway.className}>
             <Component {...pageProps} />
           </main>
