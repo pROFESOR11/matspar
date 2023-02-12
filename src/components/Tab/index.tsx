1e29734bf535b37d52560e291c6859218591da1e
Ali Cimen <ali_cimen_11@hotmail.com>
2023-02-12 20:00:19 +0300

    add Tab component

---
+++ src/components/Tab/index.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import cx from 'classnames'
+import styles from './tab.module.css'
+
+type TabItem = {
+  id: string
+  label: string
+  [key: string]: any
+}
+
+type TabProps = {
+  tabs: TabItem[]
+  defaultActiveTabIndex?: number
+  onTabChange?: (tab: TabItem) => void
+}
+
+export const Tab = ({ tabs, defaultActiveTabIndex, onTabChange }: TabProps) => {
+  const [activeId, setActiveId] = useState(tabs[defaultActiveTabIndex ?? 0].id)
+
+  const handleTabChange = (tab: TabItem) => {
+    onTabChange?.(tab)
+    setActiveId(tab.id)
+  }
+
+  return (
+    <ul className={styles.container}>
+      {tabs.map((tab) => {
+        const { id, label } = tab
+        const isActive = id === activeId
+        return (
+          <li
+            key={label}
+            className={cx(styles.tab_item, isActive ? 'subline' : 'subline2', {
+              [styles.active]: isActive
+            })}
+          >
+            <a onClick={() => handleTabChange(tab)}>{label}</a>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
