ecaa942ce22c2e4450a6203eb5ac907c2480215f
Ali Cimen <ali_cimen_11@hotmail.com>
2023-02-12 13:29:58 +0300

    add error handling for api routes

--- src/pages/api/autocomplete.ts
+++ src/pages/api/autocomplete.ts
@@ -1,21 +1,25 @@
 import { AutocompleteResponse } from '@/types/Autocomplete'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<AutocompleteResponse>
+  res: NextApiResponse<AutocompleteResponse | string>
 ) {
-  const { q } = req.query
-  if (!q) throw Error('Missing search params!')
-  var requestOptions = {
-    method: 'GET'
+  try {
+    const { q } = req.query
+    if (!q) throw Error()
+    let response = await axios({
+      url: `${process.env.API_URL}/autocomplete?query=${q}`,
+      method: 'GET'
+    })
+    res.status(200).json(response.data)
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      res
+        .status(error.response?.status ?? 500)
+        .send(error.response?.data ?? 'Internal Server Error.')
+    }
+    res.status(500).send('Internal Server Error.')
   }
-
-  const response = await fetch(
-    `${process.env.API_URL}/autocomplete?query=${q}`,
-    requestOptions
-  )
-
-  const { suggestions } = await response.json()
-  res.status(200).json(suggestions)
 }
--- src/pages/api/searchProducts.ts
+++ src/pages/api/searchProducts.ts
@@ -1,29 +1,39 @@
 import { Product } from '@/types/Product'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Product[]>
+  res: NextApiResponse<Product[] | string>
 ) {
-  const { q } = req.query
+  try {
+    const { q } = req.query
 
-  var myHeaders = new Headers()
-  myHeaders.append('Content-Type', 'application/json')
+    var myHeaders = new Headers()
+    myHeaders.append('Content-Type', 'application/json')
 
-  var raw = JSON.stringify({
-    slug: '/kategori',
-    query: {
-      q
+    var raw = JSON.stringify({
+      slug: '/kategori',
+      query: {
+        q
+      }
+    })
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw
     }
-  })
 
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw
+    const response = await fetch(`${process.env.API_URL}/slug`, requestOptions)
+    const products = await response.json()
+    res.status(200).json(products)
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      res
+        .status(error.response?.status ?? 500)
+        .send(error.response?.data ?? 'Internal Server Error.')
+    }
+    res.status(500).send('Internal Server Error.')
   }
-
-  const response = await fetch(`${process.env.API_URL}/slug`, requestOptions)
-  const products = await response.json()
-  res.status(200).json(products)
 }
